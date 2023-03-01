98cfcba6e2e8912812bf9f13d1bdd928122d405e
Jing li <13936656837@163.com>
2023-03-01 14:55:53 +0800

    feat:use fs readFile to realize an api

--- learning/http/http.ts
+++ learning/http/http.ts
@@ -1,14 +1,23 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/user' || pathName === '/') {
     res.end("this is user");
+  } else if (pathName === '/api') {
+    fs.readFile('./data.json', 'utf8',(err, data)=>{
+      const json = JSON.parse(data);
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html',
-      'headers':'this is header'
+      'headers': 'this is header'
     });
     res.end('<h1>error page</h1>')
   }
