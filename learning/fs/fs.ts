853f6cd0cc24e94652ac538df83b991a4105e2a8
Jing li <13936656837@163.com>
2023-03-01 15:05:51 +0800

    fix:fix undefined

--- learning/fs/fs.ts
+++ learning/fs/fs.ts
@@ -2,18 +2,20 @@ const fs = require("fs");
 
 //阻塞
 const data1 = fs.readFileSync("./data1.txt", "utf-8");
-console.log("同步"+data1);
-fs.writeFileSync("./data2.txt",data1, "utf-8");
+console.log("同步" + data1);
+fs.writeFileSync("./data2.txt", data1, "utf-8");
 console.log("同步写完了");
 
 //异步
-fs.readFile('./date1.txt', 'utf-8',
-    function (err, data)  {
-      console.log("异步"+data);
-      // fs.writeFile('./data2.txt', `${data}`, (err) => {
-      //   console.log(err);
-      // });
-      // console.log("回调写完了")
-    });
-
+fs.readFile('./data1.txt', 'utf-8',
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("异步" + data);
+      fs.writeFile('./data2.txt', `${data}`, (err) => {
+      });
+      console.log("回调写完了")
+    }
+);
 console.log("异步写完了");
