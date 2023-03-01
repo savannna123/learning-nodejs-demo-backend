13bd8b32d0ebb92fc7e0533966f3f73c45bc48bb
Jing li <13936656837@163.com>
2023-03-01 16:19:27 +0800

    refactor:use read file sync

--- learning/httpAndurl/http.ts
+++ learning/httpAndurl/http.ts
@@ -2,18 +2,19 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+//外部只执行一次
+const data = fs.readFileSync('./data.json', 'utf8');
+const jsonData = JSON.parse(data);
+//每次发请求都会执行
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/user' || pathName === '/') {
     res.end("this is user");
   } else if (pathName === '/api') {
-    fs.readFile('./data.json', 'utf8',(err, data)=>{
-      const json = JSON.parse(data);
-      res.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      res.end(data);
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
     });
+    res.end(jsonData);
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html',
