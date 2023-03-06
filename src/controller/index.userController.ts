9a8303c5adbfbfee787baa46a688a87a76168a9b
Jing li <13936656837@163.com>
2023-03-06 14:04:21 +0800

    feat:get user by name api

--- src/controller/index.userController.ts
+++ src/controller/index.userController.ts
@@ -11,3 +11,14 @@ export const getUsers = async (req:Request,res:Response):Promise<Response> =>{
    return res.status(500).json("Internal Server Error");
  }
 }
+
+export const getUserByName = async (req:Request,res:Response):Promise<Response> =>{
+  try{
+    const name = req.params.name;
+    const response:QueryResult =await pool.query('SELECT * FROM users WHERE user_name = $1',[name]);
+    return res.status(200).json(response.rows)
+  }catch (e) {
+    console.log(e);
+    return res.status(500).json(e);
+  }
+}
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,12 +1,12 @@
 import express =require('express');
 const router = express.Router();
 
-import {getUsers} from "../controller/index.userController";
+import {getUsers,getUserByName} from "../controller/index.userController";
 
 router.get('/test',(req,res)=>{res.send('hello')});
 
 router.get('/users',getUsers);
-// router.get('/users/:id',getUsers);
+router.get('/users/:name',getUserByName);
 // router.post('/users',getUsers);
 // router.patch('/users/:id',getUsers);
 // router.delete('/users/:id',getUsers);
