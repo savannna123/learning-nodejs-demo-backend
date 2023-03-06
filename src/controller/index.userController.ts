faba1f1ae1174758ea880bbc446c0d26c701fa74
Jing li <13936656837@163.com>
2023-03-06 14:19:26 +0800

    feat:create user api

--- src/controller/index.userController.ts
+++ src/controller/index.userController.ts
@@ -1,4 +1,4 @@
-import {Request,Response}  from "express";
+import {json, Request, Response} from "express";
 import {pool} from "../db/connect";
 import {QueryResult} from "pg";
 
@@ -20,5 +20,25 @@ export const getUserByName = async (req:Request,res:Response):Promise<Response> 
   }catch (e) {
     console.log(e);
     return res.status(500).json(e);
+  }
+}
+
+export const createUser = async (req:Request,res:Response):Promise<Response> => {
+  try{
+    const { username , password ,roleName} = req.body;
+    const response:QueryResult =await pool.query('INSERT INTO users (user_name,password,role_id) VALUES ($1,$2,(SELECT id FROM role where role_name = $3))',[username , password ,roleName]);
+    return res.status(200).json({
+      message:"User create successfully",
+      body:{
+        user:{
+          username,
+          password,
+          roleName
+        }
+      }
+    })
+  }catch (e){
+    console.log(e.message);
+    return res.status(500).json(e.message);
   }
 }
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,13 +1,13 @@
 import express =require('express');
 const router = express.Router();
 
-import {getUsers,getUserByName} from "../controller/index.userController";
+import {getUsers,getUserByName,createUser} from "../controller/index.userController";
 
 router.get('/test',(req,res)=>{res.send('hello')});
 
 router.get('/users',getUsers);
 router.get('/users/:name',getUserByName);
-// router.post('/users',getUsers);
+router.post('/users',createUser);
 // router.patch('/users/:id',getUsers);
 // router.delete('/users/:id',getUsers);
 module.exports=router;
